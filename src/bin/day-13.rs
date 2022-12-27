d70b0de74870e28abc52f879fd87c25716d0ecd5
Jonathan Ely <me@jonathanely.com>
2022-12-27 22:52:16 +0000

    Day 13 part one

--- src/bin/day-13.rs
+++ src/bin/day-13.rs
@@ -9,7 +9,7 @@ use nom::{
     IResult,
 };
 
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq)]
 enum Item {
     Single(u32),
     List(Vec<Item>),
@@ -40,12 +40,54 @@ fn packet_pair(input: &str) -> IResult<&str, (Item, Item)> {
     Ok((input, (first, second)))
 }
 
-fn part_one(input: &str) -> u32 {
-    let (_, packet_pairs) = separated_list0(newline, packet_pair)(input).unwrap();
-    0
+#[derive(Debug, PartialEq)]
+enum Comparison {
+    Ordered,
+    Unordered,
+    Same,
 }
 
-fn part_two(_input: &str) -> u32 {
+fn compare_items(left: &Item, right: &Item) -> Comparison {
+    match (left, right) {
+        (Item::Single(l), Item::Single(r)) => match l.cmp(r) {
+            std::cmp::Ordering::Less => Comparison::Ordered,
+            std::cmp::Ordering::Equal => Comparison::Same,
+            std::cmp::Ordering::Greater => Comparison::Unordered,
+        },
+        (Item::Single(_), Item::List(_)) => compare_items(&Item::List(vec![left.clone()]), right),
+        (Item::List(_), Item::Single(_)) => compare_items(left, &Item::List(vec![right.clone()])),
+        (Item::List(l), Item::List(r)) => {
+            for (l, r) in l.iter().zip(r) {
+                match compare_items(l, r) {
+                    Comparison::Ordered => return Comparison::Ordered,
+                    Comparison::Unordered => return Comparison::Unordered,
+                    Comparison::Same => continue,
+                }
+            }
+            match l.len().cmp(&r.len()) {
+                std::cmp::Ordering::Less => Comparison::Ordered,
+                std::cmp::Ordering::Equal => Comparison::Same,
+                std::cmp::Ordering::Greater => Comparison::Unordered,
+            }
+        }
+    }
+}
+
+fn part_one(input: &str) -> usize {
+    let (_, packet_pairs) = separated_list0(newline, packet_pair)(input).unwrap();
+
+    let mut ordered_indexes = Vec::new();
+
+    for (index, (left, right)) in packet_pairs.into_iter().enumerate() {
+        if compare_items(&left, &right) != Comparison::Unordered {
+            ordered_indexes.push(index + 1);
+        }
+    }
+
+    ordered_indexes.into_iter().sum()
+}
+
+fn part_two(_input: &str) -> usize {
     0
 }
 
